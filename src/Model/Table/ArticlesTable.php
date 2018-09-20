17a0b3d1354702d796a16eccc1b15602076ac215
Weston Anderson <weston.anderson.p@gmail.com>
2018-09-20 09:26:26 -0700

    Added modified default input validation for adding an Article.

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -8,6 +8,9 @@
 // src/Model/Table/Articles.php
 namespace App\Model\Table;
 
+// Allow class access to the validator.
+use Cake\Validation\Validator;
+
 use Cake\ORM\Table;
 // the Text class
 use Cake\Utility\Text;
@@ -28,5 +31,22 @@ class ArticlesTable extends Table
             // trim slug to maximum length defied in schema
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    // Allows Validation of Table entities by default when the save() function is called.
+    // For further examples on validation see:
+    // https://book.cakephp.org/3.0/en/orm/validation.html#validating-request-data
+    public function validationDefault(Validator $validator)
+    {
+        // Validate title and body so they can't be empty and validate input length.
+        $validator
+            ->notEmpty('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmpty('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
